a0362376e40cd8c31277a8617eeb8df56578137f
clementreiffers <reiffersclement@gmail.com>
2021-02-10 08:06:02 +0100

    commentaires ajoutés

--- ranger.sh
+++ ranger.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
 compter_points() {
+	# fonction pour prendre en compte le dernier point (pour ignorer les autres)
 	str=$1
 	b=$(awk -F'.' '{ print NF }' <<< $str)
 	echo $((b-2))
 }
 
 create_dir() {
-if ! [ -d $1 ]; then 
-	mkdir $1
-fi
+	# on cree le fichier s'il n'existe pas uniquement
+	if ! [ -d $1 ]; then 
+		mkdir $1
+	fi
 }
 
 repeatChar() { 
+	# on recupere le nombre de points dans le nom de fichier
+	# on met le même nombre de #-1 pour n'avoir que son extention
     input="#" 
     count=$(compter_points $1) 
     myString="$(printf "%${count}s")" 
     echo "${myString// /$input}" 
 } 
 
-
+# on test tous les éléments d'un fichier et on tri seulement si c'est un fichier
 
 for FILE in *
 do
 	if [ -f "$FILE" ]; then
+		# on supprime les espaces du nom de fichier pour que ça soit plus pratique à l'avenir
 		a=$(echo $FILE | tr "[:blank:]" "_")
 		mv "$FILE" $a
 		nbr_dieses=$(repeatChar $a)*.
--- uwo.sh
+++ uwo.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 if [ $# == 1 ]; then	
+	# si on a une option après la commande
+	# on regarde si les applications qui commencent par l'occurrence (donné en parametre) sont installées
 	sudo apt search $1 > a.log
 	nombre_ligne=$(cat a.log | wc -l);
 	nombre_ligne=$((nombre_ligne-2));
 	tail -$nombre_ligne a.log > b.log
 	rm a.log
 
-
+	# on fait un tri dans tous ceux qui sont installés
 	a=$(sed -n -e '/installé/{p;n;}' b.log)
 	echo $a > b.log
 
@@ -18,7 +20,7 @@ if [ $# == 1 ]; then
 	echo $a > b.log
 
 
-
+	# on recupere le nom du package et on le desinstalle
 	for i in $a
 	do 
 		if [ $(grep $1 <<<$i) ]; then
@@ -31,6 +33,7 @@ if [ $# == 1 ]; then
 	sudo apt autoremove -y > b.log
 	rm b.log
 else
+	# si le nombre d'éléments est insuffisant ou plus grand que 1, on explique comment utiliser
 	echo -e "\033[01m\033[47m\033[31mMauvaise utilisation\033[00m"
 	echo "Uninstall With Occurrence (UWO) : Ce script permet de désinstaller tous les logiciels qui commencent par une certaine occurence"
 	echo "Usage: ./uwo.sh <occurence>"
